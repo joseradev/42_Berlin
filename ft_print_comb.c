9835bc9c0c31c25be28b5cc2fc4998317eb91387
joseradev <joserambr@gmail.com>
2024-10-31 15:35:52 +0100

    added ft_print_comb.c

---
+++ ft_print_comb.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joseradev <joseradev@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/31 14:57:44 by joseradev         #+#    #+#             */
+/*   Updated: 2024/10/31 15:35:32 by joseradev        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb(void)
+{
+	char	x;
+	char	y;
+	char	z;
+
+	x = '0';
+	while (x <= '7')
+	{
+		y = x + 1;
+		while (y <= '8')
+		{
+			z = y + 1;
+			while (z <= '9')
+			{
+				ft_putchar(x);
+				ft_putchar(y);
+				ft_putchar(z);
+				if (!(x == '7' && y == '8' && z == '9'))
+					write (1, ", ", 2);
+				z++;
+			}
+			y++;
+		}
+		x++;
+	}
+}
+/*
+int	main(void)
+{
+	ft_print_comb();
+	ft_putchar('\n');
+}
+*/
