9ae4381470e7789b3cdaab771f56f2cc444eb9ea
joseradev <joserambr@gmail.com>
2024-10-31 11:13:37 +0100

    added ft_print_alphabet.c

---
+++ ft_print_alphabet.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_alphabet(void)
+{
+	char	alpha;
+
+	alpha = 'a';
+	while (alpha <= 'z')
+	{
+		ft_putchar(alpha);
+		alpha++;
+	}
+}
+/*
+int	main(void)
+{
+	ft_print_alphabet();
+	ft_putchar('\n');
+}
+*/
