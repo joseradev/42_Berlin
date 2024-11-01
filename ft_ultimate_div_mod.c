a525676ed17490c7c7d5b5c1f51e5be6fdf946c2
joseradev <joserambr@gmail.com>
2024-11-01 16:50:00 +0100

    divides a by b and stores the result in the int pointed to by a, and the remainder in the int pointed to by b

--- ft_ultimate_div_mod.c
+++ ft_ultimate_div_mod.c
@@ -6,31 +6,31 @@
 /*   By: joseradev <joseradev@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 15:54:09 by joseradev         #+#    #+#             */
-/*   Updated: 2024/11/01 15:54:34 by joseradev        ###   ########.fr       */
+/*   Updated: 2024/11/01 16:49:10 by joseradev        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void    ft_ultimate_div_mod(int *a, int *b)
+void	ft_ultimate_div_mod(int *a, int *b)
 {
-    int div;
-    int mod;
+	int	div;
+	int	mod;
 
-    div = *a / *b;
-    mod = *a % *b;
-    *a = div;
-    *b = mod;  
+	div = *a / *b;
+	mod = *a % *b;
+	*a = div;
+	*b = mod;
 }
 /*
 #include <stdio.h>
 
-int main(void)
+int	main(void)
 {
-    int a;
-    int b;
+	int a;
+	int b;
 
-    a = 10;
-    b = 2;
-    ft_ultimate_div_mod(&a, &b);
-    printf("Result: %d | Remainder: %d\n", a, b);
+	a = 10;
+	b = 2;
+	ft_ultimate_div_mod(&a, &b);
+	printf("Result: %d | Remainder: %d\n", a, b);
 }
 */
