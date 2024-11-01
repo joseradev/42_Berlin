510257ba1b98fcdbfbe7c6ef25528838cc9d5d9b
joseradev <joserambr@gmail.com>
2024-11-01 11:32:28 +0100

    multiple levels of pointers to set value of an int to 42

---
+++ ft_ultimate_ft.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joseradev <joseradev@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/01 10:46:59 by joseradev         #+#    #+#             */
+/*   Updated: 2024/11/01 11:30:58 by joseradev        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	int	nbr;
+	int	*ptr1;
+	int	**ptr2;
+	int	***ptr3;
+	int	****ptr4;
+	int	*****ptr5;
+	int	******ptr6;
+	int	*******ptr7;
+	int	********ptr8;
+	int	*********ptr9;
+
+	ptr1 = &nbr;
+	ptr2 = &ptr1;
+	ptr3 = &ptr2;
+	ptr4 = &ptr3;
+	ptr5 = &ptr4;
+	ptr6 = &ptr5;
+	ptr7 = &ptr6;
+	ptr8 = &ptr7;
+	ptr9 = &ptr8;
+	ft_ultimate_ft(ptr9);
+	printf("%d\n", nbr);
+
+	return (0);
+}
+*/
