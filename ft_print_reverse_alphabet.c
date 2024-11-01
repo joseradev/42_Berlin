fc0df80e7286dcc47564ded5a7de669505ec164e
joseradev <joserambr@gmail.com>
2024-11-01 11:42:05 +0100

    displays the alphabet in lowercase by descending order

--- ft_print_reverse_alphabet.c
+++ ft_print_reverse_alphabet.c
@@ -6,7 +6,7 @@
 /*   By: joseradev <joseradev@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 14:54:58 by joseradev         #+#    #+#             */
-/*   Updated: 2024/10/31 14:54:59 by joseradev        ###   ########.fr       */
+/*   Updated: 2024/11/01 11:41:40 by joseradev        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,5 +33,7 @@ int	main(void)
 {
 	ft_print_reverse_alphabet();
 	ft_putchar('\n');
+
+	return (0);
 }
 */
